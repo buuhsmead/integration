ef257817731b7e7496930538ff22e3cb45d6326d
pberkhout <patrick.berkhout@trivento.nl>
2014-11-14 09:01:18 +0100

    blueprint xml

--- camel/src/main/java/integration/camel/csv2soap/Csv2SoapRoute.java
+++ camel/src/main/java/integration/camel/csv2soap/Csv2SoapRoute.java
@@ -17,12 +17,16 @@ public class Csv2SoapRoute extends RouteBuilder {
 	private static final String PASSWORD = "password";
 	private static final String LINE = "line";
 	private static final String REQUEST_MESSAGE = "RequestMessage";
+	private static final String CSV_TO_SOAP_MAIN_ROUTE = "csv2soap-main";
+	private static final String CSV_TO_SOAP_LOG_FAIL = "csv2soap-log-fail";
+	private static final String CSV_TO_SOAP_LOG_SUCCESS = "csv2soap-log-success";
+	private static final String CSV_TO_SOAP_PROCESS_CSV_LINE = "csv2soap-process-csv-line";
 
 	@Override
 	public void configure() throws Exception {
 		CsvDataFormat csv = new CsvDataFormat();
-		from("file:{{import.dir}}/?include=.*.CSV|.*.csv")
-
+		from("file:{{import.dir}}/?delay=5000&include=.*.CSV|.*.csv")
+				.routeId(CSV_TO_SOAP_MAIN_ROUTE)
 				.setProperty(
 						FILENAME,
 						regexReplaceAll(simple("${file:name}"), "\\..*",
@@ -44,10 +48,10 @@ public class Csv2SoapRoute extends RouteBuilder {
 				.log("FILENAME: ${header." + FILENAME + "}"
 						+ ", DATATYPE: ${header." + DATATYPE + "}"
 						+ ", TIMESTAMP: ${header." + TIMESTAMP + "}")
-				.split(body().tokenize("\n")).streaming().to("direct:process")
-				;
+				.split(body().tokenize("\n")).streaming().to("direct:process");
 
 		from("direct:fail")
+				.routeId(CSV_TO_SOAP_LOG_FAIL)
 				.log("FAIL: ${body}")
 				.setHeader("CamelFileName",
 						simple("${property." + FILENAME + "}.fail"))
@@ -55,6 +59,7 @@ public class Csv2SoapRoute extends RouteBuilder {
 				.to("file:{{output.dir}}?fileExist=Append");
 
 		from("direct:success")
+				.routeId(CSV_TO_SOAP_LOG_SUCCESS)
 				.log("SUCCESS: ${body}")
 				.setHeader("CamelFileName",
 						simple("${property." + FILENAME + "}.success"))
@@ -62,6 +67,7 @@ public class Csv2SoapRoute extends RouteBuilder {
 				.to("file:{{output.dir}}?fileExist=Append");
 
 		from("direct:process")
+				.routeId(CSV_TO_SOAP_PROCESS_CSV_LINE)
 				.setProperty(LINE, simple("${body}"))
 				.unmarshal(csv)
 				.transform(simple("${body[0]}"))
@@ -83,11 +89,9 @@ public class Csv2SoapRoute extends RouteBuilder {
 				.log("HTTP_METHOD: ${headers." + Exchange.HTTP_METHOD + "}")
 				.log("${body}")
 				.recipientList(simple("${property." + URL + "}"))
-				.convertBodyTo(String.class)
-				.choice()
+				.convertBodyTo(String.class).choice()
 				.when(body().contains("faultcode")).to("direct:fail")
-				.otherwise().to("direct:success")
-				;
+				.otherwise().to("direct:success");
 
 	}
 }
